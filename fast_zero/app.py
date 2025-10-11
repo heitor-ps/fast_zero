a23210ddf274a80559bc4f210fd693d334554e15
Heitor Pereira <heytorpg7@gmail.com>
2025-10-11 19:30:52 -0300

    added dumb stuff 🌟

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Disgrasa, Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/disgrasa', status_code=HTTPStatus.OK, response_model=Disgrasa)
+def call_disgrasa():
+    return {'disgrasa': 'pudinzao disgrasadao'}
