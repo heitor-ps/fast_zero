e6c4595d914d08d6daf929f2c74260a4ef456472
Heitor Pereira <heytorpg7@gmail.com>
2025-10-24 09:22:21 -0300

    implemented password hashing and token endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -10,11 +11,16 @@ from fast_zero.models import User
 from fast_zero.schemas import (
     Disgrasa,
     Message,
+    Token,
     UserList,
     UserPublic,
     UserSchema,
 )
-from fast_zero.security import get_password_hash
+from fast_zero.security import (
+    create_access_token,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
@@ -84,6 +90,31 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     session.refresh(db_user)
 
     return db_user
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    """Returns the access token"""
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -2,14 +2,20 @@ from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class Message(BaseModel):
+    """Default message bearer"""
+
     message: str
 
 
 class Disgrasa(BaseModel):
+    """Silly stuff"""
+
     disgrasa: str
 
 
 class UserSchema(BaseModel):
+    """Internal UserSchema"""
+
     username: str
     email: EmailStr
     password: str
@@ -24,3 +30,8 @@ class UserPublic(BaseModel):
 
 class UserList(BaseModel):
     users: list[UserPublic]
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
