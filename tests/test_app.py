8e2a46d1ce7c01e655aec8c309a764c55e675273
Heitor Pereira <heytorpg7@gmail.com>
2025-11-01 14:56:32 -0300

    moved some tests to auth

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,47 +1,8 @@
 from http import HTTPStatus
 
-from fast_zero.security import create_access_token
-
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
-
-
-def test_get_current_user_not_found(client):
-    data = {'no-email': 'test'}
-    token = create_access_token(data)
-
-    response = client.delete(
-        '/users/1', headers={'Authorization': f'Bearer {token}'}
-    )
-
-    assert response.status_code == HTTPStatus.UNAUTHORIZED
-    assert response.json() == {'detail': 'Could not validate credentials'}
-
-
-def test_get_current_user_does_not_exist(client):
-    data = {'sub': 'test@test'}
-    token = create_access_token(data)
-
-    response = client.delete(
-        '/users/1',
-        headers={'Authorization': f'Bearer {token}'},
-    )
-
-    assert response.status_code == HTTPStatus.UNAUTHORIZED
-    assert response.json() == {'detail': 'Could not validate credentials'}
-
-
-def test_get_token(client, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},
-    )
-    token = response.json()
-
-    assert response.status_code == HTTPStatus.OK
-    assert 'access_token' in token
-    assert 'token_type' in token
